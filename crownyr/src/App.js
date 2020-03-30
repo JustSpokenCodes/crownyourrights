9ab19f5ec9d34bd635291bc9812df922e48a288e
JustSpokenCodes <JustSpokenCodes@users.noreply.github.com>
2020-03-29 22:16:42 -0700

    putting up the coming soon page

--- crownyr/src/App.js
+++ crownyr/src/App.js
@@ -5,20 +5,17 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <p>BeNice</p>
       </header>
+        <p>
+          Coming Soon...
+          <br></br>
+          <b>C</b><i>rown</i> <b>Y</b><i>our</i> <b>R</b><i>ights</i>
+        </p>
+      <footer>
+        
+      </footer>
     </div>
   );
 }
