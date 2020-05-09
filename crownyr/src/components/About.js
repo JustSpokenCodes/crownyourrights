570a598ad45b67f6ac5f0a5fcd9c5b4db2103fd3
JustSpokenCodes <JustSpokenCodes@users.noreply.github.com>
2020-05-09 00:21:29 -0700

    export default About

--- crownyr/src/components/About.js
+++ crownyr/src/components/About.js
@@ -5,9 +5,9 @@ const About = () => {
     const team = [
         {
             name: 'Briana E.L.H. Stevens',
-            profile: '#'
+            profile: '#',
             img: './img/maleficentStitch.jpg',
-            role: 'President/ CEO'
+            role: 'President/ CEO',
         }
     ];
 
@@ -15,7 +15,32 @@ const About = () => {
         <div className="page-wrap">
             <section className='card'>
                 <h1 className='card-title'> So You Want To Know Who I Am</h1>
+                <hr />
+                <p>
+                    Briana Stevens is one that cannot be held within one word. Dressed with a Bachelors of the Arts degree from the illustrious HBCU Howard University and a Masters of Education degree from the fantastic State of Western California. With a way of bringing passion and compassion into everything she puts her heart into, you can be Sure Briana is the Real Deal.
+                </p>
+            </section>
+            <section className='card'>
+                <h1 className='card-title'> The Women </h1>
+                <hr />
+                <div className='team'>
+                    {
+                        team.map(bs => {
+                            return <div key={bs.name}>
+                                <img className='profile-avi' src={bs.img} alt='the women' />
+                                <p className='text-bold'>
+                                    <a href={bs.profile} target="_blank" rel="noopener noreferrer">
+                                    {bs.name}
+                                    </a>
+                                    <p className='about-role'>{bs.role}</p>
+                                </p>
+                            </div>
+                        })
+                    }
+                </div>
             </section>
         </div>
     )
 }
+
+export default About;
