e532474ec7616170c4f5a9b6086501e7119f2d33
JustSpokenCodes <JustSpokenCodes@users.noreply.github.com>
2020-05-09 12:02:37 -0700

    clean up and out app.js

--- crownyr/src/App.js
+++ crownyr/src/App.js
@@ -1,31 +1,28 @@
 import React from 'react';
-import './App.css';
-import { Link, Route } from "react-router-dom";
+import { Link, Route, Switch } from "react-router-dom";
 
 //components
-//import Home from "./components/Home";
+import Home from "./components/Home";
 import About from "./components/About";
-//import Contact from "./components/Contact"
+import Contact from "./components/Contact";
 
+import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header>
-        <div className="logo">
-          <h1>BeNice</h1>
-        </div>
         <nav className='app-nav'>
-          <a href="https://www.crownyourrights.com/">Home</a>
+          <Link to='/'>Home</Link>
           <Link to='/about'>About</Link>
-          <a href="#">Contact</a>
+          <Link to='/contact'>Contact</Link>
         </nav>
       </header>
-        <p>Coming Soon...</p>
-          <h1><b>C</b><i>rown</i> <b>Y</b><i>our</i> <b>R</b><i>ights</i></h1>
-      <footer>
-        
-      </footer>
+      <Switch>
+        <Route exact path='/' component={Home} />
+        <Route path='/about' component={About} />
+        <Route path='/contact' component={Contact} />
+      </Switch>
     </div>
   );
 }
