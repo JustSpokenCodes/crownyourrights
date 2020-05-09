1ff37c206d6f680600e9a6e31867781ac4912295
JustSpokenCodes <JustSpokenCodes@users.noreply.github.com>
2020-05-09 01:17:17 -0700

    css for home.js

--- crownyr/src/components/Home.js
+++ crownyr/src/components/Home.js
@@ -13,11 +13,11 @@ const Home = () => {
             <a href="#">Contact</a>
         </nav>
         </header>
-        <p>Coming Soon...</p>
+        <section>
+            <p>Coming Soon...</p>
             <h1><b>C</b><i>rown</i> <b>Y</b><i>our</i> <b>R</b><i>ights</i></h1>
-        <footer>
-        
-        </footer>
+            <button id="custom-btn" class='crown-button'><span>Come</span>And Lets Work</button>
+        </section>
     </div>
     )
 }
