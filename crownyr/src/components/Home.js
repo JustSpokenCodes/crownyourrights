85cb76483524ede0ff97273ab08b6bfe267e4ac2
JustSpokenCodes <JustSpokenCodes@users.noreply.github.com>
2020-05-10 23:12:03 -0700

    fixed the pic and removed unneeded info in css

--- crownyr/src/components/Home.js
+++ crownyr/src/components/Home.js
@@ -3,17 +3,22 @@ import React from "react";
 const Home = () => {
     return (
         <div className="App">
-        <header>
-        <div className="logo">
-            <h1>BeNice</h1>
+            <header>
+                <div className="logo">
+                    <h1>BeNice</h1>
+                </div>
+            </header>
+            <section>
+                <p>Coming Soon...</p>
+                <h1><b>C</b><i>rown</i> <b>Y</b><i>our</i> <b>R</b><i>ights</i></h1>
+                
+            </section>
+            <footer>
+                <div className="footer">
+                    <h3><b>C</b><i>rown</i> <b>Y</b><i>our</i> <b>R</b><i>ights</i> goal is to give support and guidance to youth adults on being independent through mentorship.</h3>
+                </div>
+            </footer>
         </div>
-        </header>
-        <section>
-            <p>Coming Soon...</p>
-            <h1><b>C</b><i>rown</i> <b>Y</b><i>our</i> <b>R</b><i>ights</i></h1>
-            <button id="custom-btn" class='crown-button'><span>Come</span> And Lets Work</button>
-        </section>
-    </div>
     )
 }
 
